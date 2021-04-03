b8e1d94966ef40744070f6694b50ec9c61d72a17
takeyuki-i <golden_eggs7777@yahoo.co.jp>
2021-04-03 13:43:30 +0900

    ユーザー登録

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+before_action :devise_patameters, if: :devise_controller?
+
+private
+def devise_patameters
+  devise_parameter_sanitizer.permit(:sign_up,keys: [:name,:address] )
+end
+
 end
