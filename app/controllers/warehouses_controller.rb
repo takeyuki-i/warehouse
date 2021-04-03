53e1be31e6758fa16c9f095149bd5b5864463473
takeyuki-i <golden_eggs7777@yahoo.co.jp>
2021-04-03 16:07:43 +0900

    仮トップページとユーザー登録機能へのリンク

---
+++ app/controllers/warehouses_controller.rb
@@ -0,0 +1,5 @@
+class WarehousesController < ApplicationController
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'devise/registrations#new'
+  root to: 'warehouses#index'
+  resources :warehouses, only: :index
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
